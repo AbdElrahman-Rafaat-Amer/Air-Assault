2b7635a1029012ade03d5735875d48db5d0f8b80
AbdElRahman Rafaat Amer <abdelrahman.raafaat@gmail.com>
2023-08-07 19:48:14 +0300

    Initial Commit

---
+++ AirAssault/AirAssaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirAssaultApp.swift
+//  AirAssault
+//
+//  Created by Abdelrahman Rafaat on 07/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct AirAssaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
