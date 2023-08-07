f537bbb5a08043589c9e91be89bedf2a11e6bf50
AbdElRahman Rafaat Amer <abdelrahman.raafaat@gmail.com>
2023-08-07 20:43:56 +0300

    init commit

---
+++ AirAssault/GameView.swift
@@ -0,0 +1,20 @@
+//
+//  GameView.swift
+//  AirAssault
+//
+//  Created by Abdelrahman Rafaat on 07/08/2023.
+//
+
+import SwiftUI
+
+struct GameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GameView_Previews: PreviewProvider {
+    static var previews: some View {
+        GameView()
+    }
+}
