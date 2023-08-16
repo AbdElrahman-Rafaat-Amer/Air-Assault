8bae62ac60f102cf3509073ce569a48e0f5865f4
AbdElRahman Rafaat Amer <abdelrahman.raafaat@gmail.com>
2023-08-16 20:05:41 +0300

    [AA][IOS] Calculate Points
    [AA][IOS] Calculate Points
    https://abdelrahmanraafat.atlassian.net/browse/AA-6
    
    WorkDone:
    ========
    - Add points label to view.
    - Calculate points when a bullet hit the enemy based on enemy level.

--- AirAssault/GameView.swift
+++ AirAssault/GameView.swift
@@ -9,11 +9,22 @@ import SwiftUI
 import _SpriteKit_SwiftUI
 
 struct GameView: View {
+    @State private var points = 0
     var body: some View {
         GeometryReader { proxy in
-            VStack() {
-                let scene = GameScene(size: proxy.size)
-                SpriteView(scene: scene).frame(width: proxy.size.width, height: proxy.size.height)
+            ZStack(alignment: .topLeading) {
+                let gameScene = GameScene(size: proxy.size)
+                let _ = gameScene.gameDelegate = self
+                SpriteView(scene: gameScene).frame(width: proxy.size.width, height: proxy.size.height)
+                let _ = gameScene.view?.showsFPS = true
+                let _ = gameScene.view?.showsFields = true
+                let _ = gameScene.view?.showsPhysics = true
+                let _ = gameScene.view?.showsNodeCount = true
+                HStack(){
+                    Text("Points:").font(.system(size: 25)).foregroundColor(Color.white)
+                    
+                    Text("\(points)").font(.system(size: 25)).foregroundColor(Color.white)
+                }.padding(EdgeInsets(top: 10, leading: 10, bottom: 0, trailing: 0))
             }
             .frame(
                 minWidth: 0,
@@ -25,6 +36,12 @@ struct GameView: View {
             .background(Color.green)
             .navigationBarHidden(true)
         }
+    }
+}
+
+extension GameView : GameProtocol{
+    func onGetPoints(points: Int) {
+        self.points += points
     }
 }
 
